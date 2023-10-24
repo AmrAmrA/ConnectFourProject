f3327a9c335c5d20beeb7df0b8b29ca2d43af1df
AmrAmrA <amrelfattah.dev@gmail.com>
2023-10-24 11:31:22 +0200

    add error message toast

--- app.js
+++ app.js
@@ -10,15 +10,24 @@ function placeToken(hole, player) {
   hole.classList.add(player);
 }
 
+const messageError = document.querySelector(".messageError");
+function ErrorMessageAppearence() {
+  messageError.classList.add("active-animation");
+}
+messageError.addEventListener("animationend", () => {
+  messageError.classList.remove("active-animation");
+});
+
 const columns = document.querySelectorAll(".column");
 let currentPlayer = "playerOne";
-
 for (const column of columns) {
   column.addEventListener("click", () => {
     const lastEmptyHole = getLastEmptyHole(column);
     if (lastEmptyHole) {
       placeToken(lastEmptyHole, currentPlayer);
       currentPlayer = switchPlayer(currentPlayer);
+    } else {
+      ErrorMessageAppearence();
     }
   });
 }
