ec501c299379d12b628f7d6deb905c90df6cf80a
AmrAmrA <amrelfattah.dev@gmail.com>
2023-10-24 16:54:25 +0200

    counting every player move

--- app.js
+++ app.js
@@ -2,32 +2,58 @@ function getLastEmptyHole(column) {
   const holesArray = Array.from(column.children);
   return holesArray.findLast((element) => element.classList.contains("empty"));
 }
-function switchPlayer(currentPlayer) {
-  return currentPlayer === "playerOne" ? "playerTwo" : "playerOne";
-}
+
 function placeToken(hole, player) {
   hole.classList.remove("empty");
   hole.classList.add(player);
 }
 
+const game = {
+  currentPlayer: "playerOne",
+  playerOneMove: 0,
+  playerTwoMove: 0,
+  globalMoves: 0,
+  countMoves() {
+    this.globalMoves++;
+    this.currentPlayer === "playerOne"
+      ? this.playerOneMove++
+      : this.playerTwoMove++;
+  },
+  switchPlayer() {
+    if (this.currentPlayer === "playerOne") {
+      this.currentPlayer = "playerTwo";
+    } else {
+      this.currentPlayer = "playerOne";
+    }
+  },
+};
+
 const messageError = document.querySelector(".messageError");
 function ErrorMessageAppearence() {
   messageError.classList.add("active-animation");
+  messageError.addEventListener("animationend", () => {
+    messageError.classList.remove("active-animation");
+  });
 }
-messageError.addEventListener("animationend", () => {
-  messageError.classList.remove("active-animation");
-});
+
 
 const columns = document.querySelectorAll(".column");
-let currentPlayer = "playerOne";
+function handleColumnClick(column) {
+  const lastEmptyHole = getLastEmptyHole(column);
+
+  if (lastEmptyHole) {
+    placeTokenInColumn(lastEmptyHole);
+  } else {
+    ErrorMessageAppearence();
+  }
+}
+
+function placeTokenInColumn(hole) {
+  placeToken(hole, game.currentPlayer);
+  game.countMoves();
+  game.switchPlayer();
+}
+
 for (const column of columns) {
-  column.addEventListener("click", () => {
-    const lastEmptyHole = getLastEmptyHole(column);
-    if (lastEmptyHole) {
-      placeToken(lastEmptyHole, currentPlayer);
-      currentPlayer = switchPlayer(currentPlayer);
-    } else {
-      ErrorMessageAppearence();
-    }
-  });
+  column.addEventListener("click", () => handleColumnClick(column));
 }
